3dc0915669a19a24b2f176803c5f3a79323962fa
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-11-01 22:18:56 -0400

    feat: implement count_characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 
 def get_book_text(filepath: str) -> str:
@@ -12,7 +12,9 @@ def main() -> None:
     frankenstein_path = "books/frankenstein.txt"
     frankenstein_txt = get_book_text(frankenstein_path)
     num_words = count_words(frankenstein_txt)
+    num_chars = count_characters(frankenstein_txt)
     print(f"Found {num_words} total words")
+    print(num_chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def count_words(text: str) -> int:
     return len(text.split())
+
+
+def count_characters(text: str) -> dict[str, int]:
+    counts: dict[str, int] = {}
+    for c in text.lower():
+        if c in counts:
+            counts[c] += 1
+        else:
+            counts[c] = 1
+
+    return counts
